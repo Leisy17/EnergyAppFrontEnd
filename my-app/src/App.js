0378f53a18087e3ef01c74725eafd6e6eae6009b
Leisy17 <leisysado@gmail.com>
2021-11-26 12:19:13 -0500

    Modificar plantilla energía

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-
+import energyapp from './assets/imgs/energyapp1.png';
 function App() {
   return (
     <div className="App">
@@ -22,13 +22,14 @@ function App() {
             {/*<!-- Navigation-->*/}
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container px-5">
-                    <a className="navbar-brand" href="index.html">EnergyApp</a>
+                    <a className="navbar-brand" href="index.html"><img className="img-fluid rounded-3 my-5" src={energyapp} alt="..." /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item"><a className="nav-link" href="index.html">Inicio</a></li>
                             <li className="nav-item"><a className="nav-link" href="pricing.html">Contacto</a></li>
                             <li className="nav-item"><a className="nav-link" href="faq.html">Acceder</a></li>
+
                         </ul>
                     </div>
                 </div>
@@ -39,70 +40,18 @@ function App() {
                     <div className="row gx-5 align-items-center justify-content-center">
                         <div className="col-lg-8 col-xl-7 col-xxl-6">
                             <div className="my-5 text-center text-xl-start">
-                                <h1 className="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
-                                <p className="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                                <h1 className="display-5 fw-bolder text-white mb-2">Gestiona el uso de la Energía  en tu Planta</h1>
+                                <p className="lead fw-normal text-white-50 mb-4">Sólo inserta tus datos de consumo/producción y con  un par de clicks encontrarás el análisis energético</p>
                                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                    <a className="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                                    <a className="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                                    <a className="btn btn-primary btn-lg px-4 me-sm-3" href="#!">Inicia aquí</a>
+                                    <a className="btn btn-outline-light btn-lg px-4" href="#features">Aprender más</a>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img className="img-fluid rounded-3 my-5" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                        <div className="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img className="img-fluid rounded-3 my-5"  src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
                     </div>
                 </div>
             </header>
-            {/*<!-- Features section-->*/}
-            <section className="py-5" id="features">
-                <div className="container px-5 my-5">
-                    <div className="row gx-5">
-                        <div className="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
-                        <div className="col-lg-8">
-                            <div className="row gx-5 row-cols-1 row-cols-md-2">
-                                <div className="col mb-5 h-100">
-                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                                    <h2 className="h5">Featured title</h2>
-                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div className="col mb-5 h-100">
-                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                                    <h2 className="h5">Featured title</h2>
-                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div className="col mb-5 mb-md-0 h-100">
-                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                                    <h2 className="h5">Featured title</h2>
-                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div className="col h-100">
-                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                                    <h2 className="h5">Featured title</h2>
-                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/*<!-- Testimonial section-->*/}
-            <div className="py-5 bg-light">
-                <div className="container px-5 my-5">
-                    <div className="row gx-5 justify-content-center">
-                        <div className="col-lg-10 col-xl-7">
-                            <div className="text-center">
-                                <div className="fs-4 mb-4 fst-italic">"Working with Start Bootstrap templates has saved me tons of development time when building new projects! Starting with a Bootstrap template just makes things easier!"</div>
-                                <div className="d-flex align-items-center justify-content-center">
-                                    <img className="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                    <div className="fw-bold">
-                                        Tom Ato
-                                        <span className="fw-bold text-primary mx-1">/</span>
-                                        CEO, Pomodoro
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             {/*-- Blog preview section--*/}
             <section className="py-5">
                 <div className="container px-5 my-5">
@@ -179,6 +128,38 @@ function App() {
                             </div>
                         </div>
                     </div>
+                                {/*<!-- Features section-->*/}
+            <section className="py-5" id="features">
+                <div className="container px-5 my-5">
+                    <div className="row gx-5">
+                        <div className="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
+                        <div className="col-lg-8">
+                            <div className="row gx-5 row-cols-1 row-cols-md-2">
+                                <div className="col mb-5 h-100">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
+                                    <h2 className="h5">Featured title</h2>
+                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div className="col mb-5 h-100">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                                    <h2 className="h5">Featured title</h2>
+                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div className="col mb-5 mb-md-0 h-100">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 className="h5">Featured title</h2>
+                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div className="col h-100">
+                                    <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 className="h5">Featured title</h2>
+                                    <p className="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>   
                     {/*<!-- Call to action-->*/}
                     <aside className="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
                         <div className="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
@@ -220,3 +201,4 @@ function App() {
 }
 
 export default App;
+
